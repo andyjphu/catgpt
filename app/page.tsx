ac6fe806f0a8b27fb96c64737fe78025b02e4628
AndyP <malicioussoap@gmail.com>
2023-12-04 14:35:55 -0600

    revert by hand back to original

--- app/page.tsx
+++ app/page.tsx
@@ -23,14 +23,14 @@ export default function Home() {
           <div tabIndex={0} className="h-[95%]">
             {outputText && <p>{outputText}</p>}
           </div>
-          <div tabIndex={1} className="rounded-lg outline outline-2 outline-white">
+          <div tabIndex={1} className="rounded-lg outline outline-2 outline-white p-3">
             <input
-              className="transparent h-10 w-[95%]"
+              className="transparent w-[95%] mr-4 "
               type="text"
               value={inputText}
               onChange={(e) => setInputText(e.target.value)}
             />
-            <button className="text-xl bg-white rounded-lg text-black items-center w-8 h-8 text-center" type="submit">↑</button>
+            <button className="text-xl bg-white rounded-lg text-black items-cente w-8 h-8 text-center" type="submit">↑</button>
           </div>
         </form>
       </div>
